63fb746d3d4862b042756ee47bdbc66d4ac86d73
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-11-29 14:40:09 +0100

    update PF_FormFieldTest.php

--- tests/phpunit/integration/includes/PF_FormFieldTest.php
+++ tests/phpunit/integration/includes/PF_FormFieldTest.php
@@ -9,16 +9,34 @@ use PHPUnit\Framework\TestCase;
  */
 class PFFormFieldTest extends TestCase {
 
-	public function testCreateFormFieldWithEmptyValues() {
-		// Mock the PFTemplateField class
-		$mockTemplateField = $this->createMock( PFTemplateField::class );
+	private $mockTemplate;
+	private $mockTemplateInForm;
+	private $mockUser;
+	private $mockTemplateField;
 
+	protected function setUp(): void {
+		parent::setUp();
+
+		// Mocking the Template object
+		$this->mockTemplate = $this->createMock( PFTemplate::class );
+
+		// Mocking TemplateInForm object
+		$this->mockTemplateInForm = $this->createMock( PFTemplateInForm::class );
+
+		// Mocking the User object
+		$this->mockUser = $this->createMock( User::class );
+
+		// Mock the PFTemplateField class
+		$this->mockTemplateField = $this->createMock( PFTemplateField::class );
+	}
+
+	public function testCreateFormFieldWithEmptyValues() {
 		// Create the PFFormField object
-		$formField = PFFormField::create( $mockTemplateField );
+		$formField = PFFormField::create( $this->mockTemplateField );
 
 		// Verify the object is an instance of PFFormField
 		$this->assertInstanceOf( PFFormField::class, $formField );
-		$this->assertSame( $mockTemplateField, $formField->template_field );
+		$this->assertSame( $this->mockTemplateField, $formField->template_field );
 
 		$this->assertNull( $formField->getInputType() );
 		$this->assertFalse( $formField->isMandatory() );
@@ -30,29 +48,26 @@ class PFFormFieldTest extends TestCase {
 	}
 
 	public function testCreateFormFieldWithTemplateFieldProps() {
-		// Mock the PFTemplateField class
-		$mockTemplateField = $this->createMock( PFTemplateField::class );
-
 		// Set expectations for the mocked methods or properties
-		$mockTemplateField->method( 'getFieldName' )->willReturn( 'MockedFieldName' );
-		$mockTemplateField->method( 'getLabel' )->willReturn( 'Mocked Label' );
-		$mockTemplateField->method( 'getSemanticProperty' )->willReturn( 'MockedSemanticProperty' );
-		$mockTemplateField->method( 'isList' )->willReturn( true );
-		$mockTemplateField->method( 'getDelimiter' )->willReturn( ';' );
-		$mockTemplateField->method( 'getDisplay' )->willReturn( 'MockedDisplay' );
+		$this->mockTemplateField->method( 'getFieldName' )->willReturn( 'MockedFieldName' );
+		$this->mockTemplateField->method( 'getLabel' )->willReturn( 'Mocked Label' );
+		$this->mockTemplateField->method( 'getSemanticProperty' )->willReturn( 'MockedSemanticProperty' );
+		$this->mockTemplateField->method( 'isList' )->willReturn( true );
+		$this->mockTemplateField->method( 'getDelimiter' )->willReturn( ';' );
+		$this->mockTemplateField->method( 'getDisplay' )->willReturn( 'MockedDisplay' );
 
 		// Create the PFFormField object
-		$formField = PFFormField::create( $mockTemplateField );
+		$formField = PFFormField::create( $this->mockTemplateField );
 
 		// Verify the object is an instance of PFFormField
 		$this->assertInstanceOf( PFFormField::class, $formField );
 
 		// Assert that the template field is set correctly
-		$this->assertSame( $mockTemplateField, $formField->template_field );
+		$this->assertSame( $this->mockTemplateField, $formField->template_field );
 
 		// Assert that properties were set correctly based on the mocked PFTemplateField
 		$this->assertSame( 'MockedFieldName', $formField->template_field->getFieldName() );
-		$this->assertSame( $mockTemplateField, $formField->getTemplateField() );
+		$this->assertSame( $this->mockTemplateField, $formField->getTemplateField() );
 		$this->assertSame( 'MockedSemanticProperty', $formField->template_field->getSemanticProperty() );
 		$this->assertSame( ';', $formField->template_field->getDelimiter() );
 		$this->assertSame( 'MockedDisplay', $formField->template_field->getDisplay() );
@@ -60,18 +75,18 @@ class PFFormFieldTest extends TestCase {
 
 	public function testDelimiterHandling() {
 		// Mock the PFTemplateField class
-		$mockTemplateFieldOne = $this->createMock( PFTemplateField::class );
-		$mockTemplateFieldTwo = $this->createMock( PFTemplateField::class );
-		$mockTemplateFieldOne->method( 'getDelimiter' )->willReturn( ',' );
-		$mockTemplateFieldTwo->method( 'getDelimiter' )->willReturn( ';' );
+		$this->mockTemplateFieldOne = $this->createMock( PFTemplateField::class );
+		$this->mockTemplateFieldTwo = $this->createMock( PFTemplateField::class );
+		$this->mockTemplateFieldOne->method( 'getDelimiter' )->willReturn( ',' );
+		$this->mockTemplateFieldTwo->method( 'getDelimiter' )->willReturn( ';' );
 
-		$mockTemplateFieldThree = $this->createMock( PFTemplateField::class );
-		$mockTemplateFieldThree->method( 'getDelimiter' )->willReturn( '' );
+		$this->mockTemplateFieldThree = $this->createMock( PFTemplateField::class );
+		$this->mockTemplateFieldThree->method( 'getDelimiter' )->willReturn( '' );
 
 		// Create the PFFormField objects
-		$fieldOne = PFFormField::create( $mockTemplateFieldOne );
-		$fieldTwo = PFFormField::create( $mockTemplateFieldTwo );
-		$fieldThree = PFFormField::create( $mockTemplateFieldThree );
+		$fieldOne = PFFormField::create( $this->mockTemplateFieldOne );
+		$fieldTwo = PFFormField::create( $this->mockTemplateFieldTwo );
+		$fieldThree = PFFormField::create( $this->mockTemplateFieldThree );
 
 		$this->assertEquals( ',', $fieldOne->template_field->getDelimiter() );
 		$this->assertEquals( ';', $fieldTwo->template_field->getDelimiter() );
@@ -85,6 +100,54 @@ class PFFormFieldTest extends TestCase {
 		if ( $delimiter != '' ) {
 			$fieldTwo->setFieldArg( 'delimiter', $fieldTwo->template_field->getDelimiter() );
 			$this->assertEquals( ';', $fieldTwo->getFieldArgs()['delimiter'] );
+		}
+	}
+
+	public function testNewFromFormFieldTag_FieldExistsInTemplate() {
+		// Mock data for the tag_components
+		$tagComponents = [ 'somePrefix', 'fieldName' ];
+
+		// Mock the template to return a field
+		$mockField = $this->createMock( PFTemplateField::class );
+		$this->mockTemplate->method( 'getFieldNamed' )->willReturn( $mockField );
+
+		// Mock the template_in_form to return a template name
+		$this->mockTemplateInForm->method( 'getTemplateName' )->willReturn( 'TestTemplate' );
+		$this->mockTemplateInForm->method( 'strictParsing' )->willReturn( false );
+
+		// Call the method under test
+		$formField = PFFormField::newFromFormFieldTag( $tagComponents, $this->mockTemplate, $this->mockTemplateInForm, false, $this->mockUser );
+
+		// Assert that the template field was set
+		$this->assertInstanceOf( PFTemplateField::class, $formField->template_field );
+		$this->assertSame( $mockField, $formField->template_field );
+	}
+
+	public function testNewFromFormFieldTag_FieldDoesNotExistInTemplate_StrictParsing() {
+		// Mock data for the tag_components
+		$tagComponents = [ 'somePrefix', 'fieldName' ];
+
+		// Mock the template to return null, meaning no field is found
+		$this->mockTemplate->method( 'getFieldNamed' )->willReturn( null );
+
+		// Mock the template_in_form to return a template name and enable strict parsing
+		$this->mockTemplateInForm->method( 'getTemplateName' )->willReturn( 'TestTemplate' );
+		$this->mockTemplateInForm->method( 'strictParsing' )->willReturn( true );
+
+		// Create the PFFormField object
+		$formField = PFFormField::create( $this->mockTemplateField );
+
+		$this->assertSame( $this->mockTemplateField, $formField->template_field );
+
+		if ( $this->mockTemplateInForm->strictParsing() ) {
+
+			$formField->template_field = new PFTemplateField();
+			$this->mockTemplateField->mIsList = false;
+
+			// Assert that mIsList is set to false if strictParsing is set to TRUE
+			$this->assertInstanceOf( PFTemplateField::class, $formField->template_field );
+			$this->assertNull( $formField->template_field->isList() );
+			$this->assertFalse( $this->mockTemplateField->mIsList );
 		}
 	}
 }
